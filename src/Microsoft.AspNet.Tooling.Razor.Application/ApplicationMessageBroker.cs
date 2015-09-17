daed20c3df7f2f871ddcc188b1c39b5c72027c0c
N. Taylor Mullen <nimullen@microsoft.com>
2015-09-16 17:20:06 -0700

    Prototype command line entry point to the Razor plugin.

---
+++ src/Microsoft.AspNet.Tooling.Razor.Application/ApplicationMessageBroker.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Dnx.DesignTimeHost;
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNet.Tooling.Razor.Application
+{
+    public class ApplicationMessageBroker : IPluginMessageBroker
+    {
+        public void SendMessage(object data)
+        {
+            var stringData = JsonConvert.SerializeObject(data);
+
+            Console.WriteLine(stringData);
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Tooling.Razor.Application/Program.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Dnx.Runtime;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.AspNet.Tooling.Razor.Application
+{
+    public class Program
+    {
+        private IAssemblyLoadContext _assemblyLoadContext;
+
+        public Program(IAssemblyLoadContextAccessor assemblyLoadContextAccessor)
+        {
+            _assemblyLoadContext = assemblyLoadContextAccessor.Default;
+        }
+
+        public void Main(string[] messages)
+        {
+            var messageBroker = new ApplicationMessageBroker();
+            var plugin = new RazorPlugin(messageBroker);
+
+            foreach (var message in messages)
+            {
+                plugin.ProcessMessage(JObject.Parse(message), _assemblyLoadContext);
+            }
+        }
+    }
+}
